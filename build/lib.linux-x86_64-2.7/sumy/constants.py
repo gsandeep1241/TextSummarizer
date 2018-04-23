990b6d076f6bf4bce93615b190b1345fc9501a15
Aditya Gujral <aditya.gujral@tamu.edu>
2018-04-22 21:26:19 -0500

    fix constants

--- build/lib.linux-x86_64-2.7/sumy/constants.py
+++ build/lib.linux-x86_64-2.7/sumy/constants.py
@@ -1,3 +1,3 @@
-PATH = '/media/aditya/extpart/IrProj/sumy-dev/DUC_data/DUC2006/duc2006_docs/'
-GtPath = '/media/aditya/extpart/IrProj/sumy-dev/DUC_data/DUC2006/NISTeval/ROUGE/peers/'
-googlePath = '/media/aditya/extpart/IrProj/GoogleNews-vectors-negative300.bin'
+PATH = '../DUC_data/DUC2006/duc2006_docs/'
+GtPath = '../DUC_data/DUC2006/NISTeval/ROUGE/peers/'
+googlePath = '../../../../../GoogleNews-vectors-negative300.bin'
